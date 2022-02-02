eac98e84bd3dde242c79c5a1d9f193a5974d9435
Elias Ferreira <eliasef0305@gmail.com>
2022-02-02 12:38:36 -0300

    Update 3. calculaIMC.js

--- 3. calculaIMC.js
+++ 3. calculaIMC.js
@@ -10,31 +10,25 @@
 // 2. Imprima o peso ideal através de ("Está entre (peso_ideal_min e peso ideal_maximo)")
 
 
-function calculaIMC(peso, altura) {
 
-    var IMCcalculado = IMCcalculado = peso / (altura ** 2)
+function calculaIMC (peso, altura) {
 
-    if (IMCcalculado <= 18.5) {
-        console.log("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "E você está ABAIXO DO PESO!");
-    } else if (IMCcalculado <= 24.9) {
-        console.log("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "E você está com o PESO NORMAL");
-    } else if (IMCcalculado <= 29.9) {
-        console.log("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "E você está com SOBREPESO");
-    } else if (IMCcalculado <= 34.9) {
-        console.log("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "E você está com OBESIDADE GRAU I");
-    } else if (IMCcalculado <= 39.9) {
-        console.log("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "E você está com OBESIDADE GRAU II");
-    } else {
-        console.log("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "E você está com OBESIDADE GRAU III");
-    }
+    var IMCcalculado = peso / (altura ** 2);
 
-    const IMC_IDEAL_MINIMO = 18.6;
-    const IMC_IDEAL_MAXIMO = 24.9;
-    var pesoIdealMinimo = IMC_IDEAL_MINIMO * (altura ** 2);
-    var pesoIdealMaximo = IMC_IDEAL_MAXIMO * (altura ** 2);
+    if (IMCcalculado <= 18.5) console.log ("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "Você está ABAIXO do PESO!")       
+    else if (IMCcalculado <= 24.9) console.log ("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "Você está com PESO NORMAL!")         
+    else if (IMCcalculado <= 29.9) console.log ("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "Você está com SOBREPESO! ")
+    else if (IMCcalculado <= 34.9) console.log ("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "Você está com OBESIDADE GRAU I") 
+    else if (IMCcalculado <= 39.9) console.log ("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "Você está com OBESIDADE GRAU II")
+    else if (IMCcalculado >= 40) console.log ("Seu IMC é de: " + IMCcalculado.toFixed(2) + "\n" + "Você está com OBESIDADE GRAU III, partiu academia?")
+           
+    const PESO_IDEAL_MINIMO = 18.6;
+    const PESO_IDEAL_MAXIMO = 24.9;
+    var pesoIdealMinimo = PESO_IDEAL_MINIMO * (altura **2);
+    var pesoIdealMaximo = PESO_IDEAL_MAXIMO * (altura **2);
+    var mediaPesos = (pesoIdealMinimo + pesoIdealMaximo) / 2; 
 
-    console.log("O seu PESO IDEAL está entre: " + pesoIdealMinimo.toFixed(2) + " e " + pesoIdealMaximo.toFixed(2))
-    
+    console.log("O seu peso ideal é de: " + mediaPesos.toFixed(2));
+
 }
-calculaIMC(70, 1.80)
-
+calculaIMC(70,1.80)
