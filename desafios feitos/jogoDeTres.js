9bf1b2437ae0048172cc29081122f576e4d4fb30
Elias Ferreira <eliasef0305@gmail.com>
2022-02-11 00:35:14 -0300

    Add files via upload

---
+++ desafios feitos/jogoDeTres.js
@@ -0,0 +1,19 @@
+function jogoDosTres (numero) {
+
+    console.log("Seu número é: " + numero)
+
+    let num1 = Math.floor(Math.random() * numero + 1)
+    console.log("Número um é: " + num1)
+
+    let num2 = Math.floor(Math.random() * numero + 1)
+    console.log("Número um é: " + num2)
+
+    let num3 = Math.floor(Math.random() * numero + 1)
+    console.log("Número um é: " + num3)
+
+    if(numero === num1 && numero === num2 && numero === num3) {
+        console.log("Você ganhou!")
+    }
+    
+}
+jogoDosTres(3)
