b7f8e42446726427c52d15563c88b6501def1e91
Elias Ferreira <eliasef0305@gmail.com>
2022-02-13 00:55:16 -0300

    Add files via upload

---
+++ desafios feitos/contarPalavras.js
@@ -0,0 +1,6 @@
+// Esta é uma função que diz quantas palavras existem em uma frase (string).
+
+function contarPalavras (str) {
+    return str.trim().split(/\s+/).length; // .trim() conta os caracteres vazios left e rigth da string // .split(/\s+\).length é um "canivete suíço" chamado "Expressão Regular", que tem 1001 utilidades. Ela entra dentro da string, e após um "espaço/caracter vazio" ela conta o seu comprimento com o .length
+}
+console.log(contarPalavras(" Olá, Mundo! Me chamo Elias. "))
