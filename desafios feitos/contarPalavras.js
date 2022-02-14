7f5d1e8586efa4775c67ff6a7adc15fa85ed8ce5
Elias Ferreira <eliasef0305@gmail.com>
2022-02-14 00:35:34 -0300

    Update contarPalavras.js

--- desafios feitos/contarPalavras.js
+++ desafios feitos/contarPalavras.js
@@ -1,4 +1,4 @@
-// Crie uma função que diz quantas palavras existem em uma frase (string).
+// Escreva uma função que diz quantas palavras existem em uma frase (string).
 
 function contarPalavras (str) {
     return str.trim().split(/\s+/).length; // .trim() conta os caracteres vazios left e rigth da string // .split(/\s+\).length é um "canivete suíço" chamado "Expressão Regular", que tem 1001 utilidades. Ela entra dentro da string, e após um "espaço/caracter vazio" ela conta o seu comprimento com o .length
