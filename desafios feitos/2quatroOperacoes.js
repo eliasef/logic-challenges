fc7f203ea6c154ea0adc2f1209b09d671accc929
Elias Ferreira <eliasef0305@gmail.com>
2022-02-20 18:50:50 -0300

    Add files via upload

---
+++ desafios feitos/2quatroOperacoes.js
@@ -0,0 +1,9 @@
+function quatroOperacoes (num1, num2) {
+
+    console.log(num1 + "+" + num2 + "=" + (num1 + num2)) 
+    console.log(num1 + "-" + num2 + "=" + (num1 - num2))
+    console.log(num1 + "*" + num2 + "=" + (num1 * num2))
+    console.log(num1 + "/" + num2 + "=" + (num1 / num2))
+
+}
+quatroOperacoes(10,5)
