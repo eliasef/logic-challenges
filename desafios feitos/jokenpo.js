c68911c8707653296fa80aa1be2752d1adf00470
Elias Ferreira <eliasef0305@gmail.com>
2022-02-20 22:33:18 -0300

    Update jokenpo.js

--- desafios feitos/jokenpo.js
+++ desafios feitos/jokenpo.js
@@ -1,9 +1,8 @@
 // Programa que simula o Jokenpo! Basta escrever sua entrada sendo uma string com letras minúsculas, chamando a função jokenpo.
 // 1. A entrada é jogador1, que deve receber a string pedra, papel ou tesoura.
 // 2. A saída deve seve imprimir se você ganhou ou perdeu.
-// O nome da função é jokenpoOffline pois você joga sozinho com a máquina.
 
-function jokenpoOffline (jogador1) {
+function jokenpo (jogador1) {
 
 if (jogador1 === "pedra" || jogador1 === "papel" || jogador1 === "tesoura") {
 
@@ -33,4 +32,4 @@ if (jogador1 === "pedra" || jogador1 === "papel" || jogador1 === "tesoura") {
     }
 }
 
-jokenpoOffline("pedra")
+jokenpo("pedra")
