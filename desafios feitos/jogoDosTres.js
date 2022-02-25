0e5876a75351781e1921b9354067c0e4839d385f
Elias Ferreira <eliasef0305@gmail.com>
2022-02-25 19:43:10 -0300

    Update jogoDosTres.js

--- desafios feitos/jogoDosTres.js
+++ desafios feitos/jogoDosTres.js
@@ -2,7 +2,7 @@ function jogoDosTres (numero) {
 
     console.log("Seu número é: " + numero)
 
-    let num1 = Math.floor(Math.random() * numero + 1)
+    let num1 = Math.floor(Math.random() * numero + 1) //gera numero aleatorio entre numero, ex: 3 pode vim 1, 2 e 3, o +1 é para conseguir pegar o 3, exemplo: math.random vai multiplicar 0,7 * 4 -> arrendonda pra 3
     console.log("Número um é: " + num1)
 
     let num2 = Math.floor(Math.random() * numero + 1)
@@ -11,10 +11,14 @@ function jogoDosTres (numero) {
     let num3 = Math.floor(Math.random() * numero + 1)
     console.log("Número um é: " + num3)
 
-    if (numero === num1 && numero === num2 && numero === num3) {
-        console.log("Você ganhou!")
-    }   else {
+    let soma = (num1 + num2 + num3) / 3 
+
+    if (soma === numero) {
+        console.log("Você ganhou o prêmio máximo!") // caso os numeros sejam todos iguais
+    } else if (num1 === num2 && num1 === num3) {
+        console.log("Você ganhou!") // caso num1, num2 e num3 sejam iguais
+    } else {
         console.log("Você perdeu!")
-    }      
+    }
 }
 jogoDosTres(3)
