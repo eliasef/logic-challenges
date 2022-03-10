12fb08a8dca8ac6c046e827332dcba16b72369a8
Elias Ferreira <eliasef0305@gmail.com>
2022-03-10 13:07:33 -0300

    Add files via upload

---
+++ tipoDeIngresso.js
@@ -0,0 +1,19 @@
+function tipoDeIngresso (informacoes) {
+    
+    if (informacoes.temIngresso === true && informacoes.idade > informacoes.censura || informacoes.estaComPais === true) {
+        if (informacoes.temCarteirinha === true || informacoes.idade < 16) {
+            console.log('Meia entrada.')
+        } else {
+            console.log('Inteira.')
+        }
+    } else {
+        console.log('Acesso negado!')
+    }
+}
+tipoDeIngresso({
+    temIngresso: true,
+    idade: 19,
+    temCarteirinha: false,
+    censura: 16,
+    estaComPais: false
+})
