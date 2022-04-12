3e994ea3db75d7e79368a5e3d4565c510e7ccde7
Elias Ferreira <eliasef0305@gmail.com>
2022-04-12 17:15:29 -0300

    Add files via upload

---
+++ desafios feitos/menorValor.js
@@ -0,0 +1,12 @@
+// 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smallerValue = numbers[0]
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < smallerValue) {
+        smallerValue = numbers[i]
+    }
+}
+console.log(smallerValue)
